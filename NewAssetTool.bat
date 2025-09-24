94445bb31958e3f246ce044f85afc6d00b627d6d
Digby Andrews <digbysa@gmail.com>
2025-09-24 11:29:44 -0700

    Update NewAssetTool.bat

--- NewAssetTool.bat
+++ NewAssetTool.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-cd /d "C:\Users\da1701_sa\Desktop\New Asset Tool"
+cd /d "C:\Users\da1701_sa\Desktop\New-Inventory-Tool"
 powershell.exe -NoProfile -ExecutionPolicy Bypass -STA -File ".\NewAssetTool.ps1"
 echo.
 echo (Done) Press any key to close...
