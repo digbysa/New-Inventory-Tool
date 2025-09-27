704d5a73d19e1a6c842d7abbc41e5363a411af4f
Digby Andrews [DA1701_SA] <digbysa@gmail.com>
2025-09-27 08:47:58 -0700

    Update NewAssetTool.ps1

--- NewAssetTool.ps1
+++ NewAssetTool.ps1
@@ -1,4 +1,4 @@
-﻿﻿Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()
 # ===== Script directory resolver (robust, PS 5.1-safe) =====
