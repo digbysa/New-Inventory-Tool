878df90e842b3b5e6775d1a03c3b014a587e7531
Digby Andrews <digbysa@gmail.com>
2025-09-28 00:13:07 -0700

    Add WPF loader host for existing inventory form

--- NewAssetTool.bat
+++ NewAssetTool.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 cd /d "C:\Users\da1701_sa\Desktop\New-Inventory-Tool"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -STA -File ".\NewAssetTool.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -STA -File ".\NewAssetTool.Wpf.ps1"
 echo.
 echo (Done) Press any key to close...
 pause >nul
